eff60c44a3b2089a7dc2b6854007d392de749787
Rainer Schuetze <r.sagitario@gmx.de>
2012-11-27 00:17:22 +0100

    merge environment64 patches avoid trying to generate debug info for string enums

--- src/mars.c
+++ src/mars.c
@@ -49,6 +49,8 @@ void obj_end(Library *library, File *objfile);
 
 void printCtfePerformanceStats();
 
+static bool parse_arch(size_t argc, char** argv, bool is64bit);
+
 Global global;
 
 Global::Global()
@@ -515,6 +517,18 @@ int tryMain(size_t argc, char *argv[])
 #else
 #error "fix this"
 #endif
+
+    size_t dflags_argc = 0;
+    char** dflags_argv = NULL;
+    getenv_setargv("DFLAGS", &dflags_argc, &dflags_argv);
+
+    char is64bit = global.params.is64bit; // use default
+    is64bit = parse_arch(argc, argv, is64bit);
+    is64bit = parse_arch(dflags_argc, dflags_argv, is64bit);
+    global.params.is64bit = is64bit;
+
+    inifile(argv[0], inifilename, is64bit ? "Environment64" : "Environment32");
+
     getenv_setargv("DFLAGS", &argc, &argv);
 
 #if 0
@@ -887,6 +901,11 @@ int tryMain(size_t argc, char *argv[])
             files.push(p);
         }
     }
+
+    if(global.params.is64bit != is64bit)
+        error(0, "the architecture must not be changed in the %s section of %s",
+              is64bit ? "Environment64" : "Environment32", inifilename);
+
     if (global.errors)
     {
         fatal();
@@ -895,8 +914,6 @@ int tryMain(size_t argc, char *argv[])
     {   usage();
         return EXIT_FAILURE;
     }
-
-    inifile(argv[0], inifilename, global.params.is64bit ? "Environment64" : "Environment32");
 
     if (!setdebuglib)
         global.params.debuglibname = global.params.defaultlibname;
@@ -1640,6 +1657,28 @@ void getenv_setargv(const char *envvar, size_t *pargc, char** *pargv)
 Ldone:
     *pargc = argc;
     *pargv = argv->tdata();
+}
+
+/***********************************
+ * Parse command line arguments for -m32 or -m64
+ * to detect the desired architecture.
+ */
+
+static bool parse_arch(size_t argc, char** argv, bool is64bit)
+{
+    for (size_t i = 0; i < argc; ++i)
+    {   char* p = argv[i];
+        if (p[0] == '-')
+        {
+            if (strcmp(p + 1, "m32") == 0)
+                is64bit = 0;
+            else if (strcmp(p + 1, "m64") == 0)
+                is64bit = 1;
+            else if (strcmp(p + 1, "run") == 0)
+                break;
+        }
+    }
+    return is64bit;
 }
 
 #if WINDOWS_SEH
